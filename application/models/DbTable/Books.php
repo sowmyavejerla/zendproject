987193a11899c97d8186fab520a7006aa3a8601b
sowmyavejerla <sowmya@innovateapps.com>
2016-07-14 16:56:45 +0530

    added application and docs folders

---
+++ application/models/DbTable/Books.php
@@ -0,0 +1,13 @@
+<?php
+class Application_Model_DbTable_Books extends Zend_Db_Table_Abstract{
+
+    protected $_name = "Books";
+    public function saveTransactionLog($input){
+
+        $db = Zend_Registry::get('db');
+
+        $select = $db->select()->from($this->_name);
+        print_r($db->fetchAll($select));
+
+    }
+}
